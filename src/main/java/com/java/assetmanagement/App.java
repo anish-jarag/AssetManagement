6eb2c5c0f9502c2f248edc6c527ae88248e165a8
Anish Jarag <anishjarag16@gmail.com>
2025-04-04 21:46:05 +0530

    All Model Class Completedgit add .

--- src/main/java/com/java/assetmanagement/App.java
+++ src/main/java/com/java/assetmanagement/App.java
@@ -1,9 +1,15 @@
 package com.java.assetmanagement;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.java.assetmanagement.util.ConnectionHelper;
+
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ClassNotFoundException, SQLException {
     System.out.println("Hello World!");
+    Connection connection = ConnectionHelper.getConnection();
   }
 
 }
